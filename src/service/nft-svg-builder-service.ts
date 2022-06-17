07bff10150f5e2ecb3a3d730ea5bab1dc85eb242
jdwstartb <jens@startbahn.jp>
2022-06-18 05:34:14 +0900

    finally tests are green

--- src/service/nft-svg-builder-service.ts
+++ src/service/nft-svg-builder-service.ts
@@ -3,7 +3,7 @@ export class NFTSVGBuilderService {
 
     async buildSVGString(params)
     {
-        return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns='http://www.w3.org/2000/svg'></svg>"
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns='http://www.w3.org/2000/svg'> </svg>"
     }
 
 }
--- src/service/test/nft-svg-builder-service.spec.ts
+++ src/service/test/nft-svg-builder-service.spec.ts
@@ -5,9 +5,9 @@ import {NFTSVGBuilderService} from "../nft-svg-builder-service";
 describe("NFTSVGBuilderService", ()=>{
     const nftSVGBuilderService = new NFTSVGBuilderService()
 
-    it("builds a basic svg", ()=>{
-        expect(true).toEqual(false)
-        const svgData = nftSVGBuilderService.buildSVGString({eyes:"buttons"})
-        expect(svgData).toMatch(/^<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns='http:\/\/www.w3.org\/2000\/svg'>.*<\/svg>$/)
+    it("builds a basic svg", async()=>{
+        const svgData = await nftSVGBuilderService.buildSVGString({eyes:"buttons"})
+
+        expect(svgData).toMatch(/^<\?xml version="1.0" encoding="UTF-8" standalone="no"\?><svg xmlns='http:\/\/www.w3.org\/2000\/svg'>.*<\/svg>$/)
     })
 })
