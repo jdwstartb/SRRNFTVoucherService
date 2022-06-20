28d16bd85a94a3716992fd0d3ea2ba3a65e95c39
jdwstartb <jens@startbahn.jp>
2022-06-21 08:43:15 +0900

    Green: getGalleryEntries

--- src/service/gallery-service.ts
+++ src/service/gallery-service.ts
@@ -2,4 +2,17 @@ export class GalleryService {
     async addIssuedSRRByWebhookV1(issuedWebhookV1): Promise<{ success: boolean }> {
         return {success: true}
     }
+
+    async getGalleryEntries(): Promise<[{
+        viewerUrl: string
+        imageUrl: string
+        tokenId: string
+    }]> {
+        return [{
+            viewerUrl: "string",
+            imageUrl: "string",
+            tokenId: "string"
+        }]
+    }
+
 }
