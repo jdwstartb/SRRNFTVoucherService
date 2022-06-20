ce5efdc700a048035e693e37e6e4af1f77d249bd
jdwstartb <jens@startbahn.jp>
2022-06-21 08:52:26 +0900

    fix misc unit tests..

--- src/service/test/metadata-service.spec.ts
+++ src/service/test/metadata-service.spec.ts
@@ -1,4 +1,5 @@
 import {MetadataService} from "../metadata-service"
+import {getValidCustomParams} from "../../custom_content/test/custom-param-factory";
 
 describe('MetadataService', () => {
     let service
@@ -12,10 +13,10 @@ describe('MetadataService', () => {
     })
     it("returns  external id and the edition number naming", () => {
         const imgUrl = "https://www.google.com/url"
-        const file = service.getMetadataRequest({}, imgUrl)
+        const file = service.getMetadataRequest(getValidCustomParams().params, imgUrl)
         expect(file).toMatchObject({
             payload: [{
-                externalId: "SBNYv1-2",
+                externalId: /^SBNYv1-2.*/,
                 metadata: {title: {en: "Startbunny SRR Season 1 #2/12"}}
             }]
         })
@@ -23,7 +24,7 @@ describe('MetadataService', () => {
 
     it("returns the given URLs at the correct position", () => {
         const imgUrl = "https://www.google.com/url"
-        const file = service.getMetadataRequest({}, imgUrl)
+        const file = service.getMetadataRequest(getValidCustomParams().params, imgUrl)
         expect(JSON.stringify(file)).toMatch(/https:\/\/www.google.com\/url/)
         expect(file).toMatchObject({payload: [{metadata: {thumbnailURL: imgUrl, image: imgUrl}}]})
     })
