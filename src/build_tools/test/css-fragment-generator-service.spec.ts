bec4ee28bb114da52a9e9ec484e8865df6e7d89b
jdwstartb <jens@startbahn.jp>
2022-06-26 19:34:07 +0900

    blue: css generator

--- src/build_tools/test/css-fragment-generator-service.spec.ts
+++ src/build_tools/test/css-fragment-generator-service.spec.ts
@@ -5,6 +5,16 @@ describe("CSSFragmentGeneratorService", () => {
 
     const service: CSSFragmentGeneratorService = new CSSFragmentGeneratorService()
 
+    function expectClassNameCorrect(cssFragment, featureName, characteristicName) {
+        const className = `.option-${featureName}-${characteristicName}`
+        expect(cssFragment).toMatch(className)
+    }
+
+    function expectUrlCorrect(cssFragment, featureName, characteristicName) {
+        const urlPath = `url(./assets/${featureName}/${featureName}-${characteristicName}.png)`
+        expect(cssFragment).toMatch(urlPath)
+    }
+
     it("generates a css fragment for a single characteristics", () => {
         const featureName = "aFeatureName"
         const characteristicsName = "aCharacteristicName"
@@ -17,11 +27,10 @@ describe("CSSFragmentGeneratorService", () => {
 
         const cssFragments = service.getCSSFragments(testFeature)
 
+
         expect(cssFragments.length).toEqual(1)
-        const className = `.option-${featureName}-${characteristicsName}`
-        expect(cssFragments[0]).toMatch(className)
-        const urlPath = `url(./assets/${featureName}/${featureName}-${characteristicsName}.png)`
-        expect(cssFragments[0]).toMatch(urlPath)
+        expectClassNameCorrect(cssFragments[0], featureName, characteristicsName)
+        expectUrlCorrect(cssFragments[0], featureName, characteristicsName)
     })
 
     it("generates css fragments for multiple characteristics", () => {
@@ -41,15 +50,12 @@ describe("CSSFragmentGeneratorService", () => {
         const cssFragments = service.getCSSFragments(testFeature)
 
         expect(cssFragments.length).toEqual(2)
-        const className = `.option-${featureName}-${characteristicsName}`
-        expect(cssFragments[0]).toMatch(className)
-        const urlPath = `url(./assets/${featureName}/${featureName}-${characteristicsName}.png)`
-        expect(cssFragments[0]).toMatch(urlPath)
+        expectClassNameCorrect(cssFragments[0], featureName, characteristicsName)
+        expectClassNameCorrect(cssFragments[1], featureName, characteristicsName2)
 
-        const className2 = `.option-${featureName}-${characteristicsName2}`
-        expect(cssFragments[1]).toMatch(className2)
-        const urlPath2 = `url(./assets/${featureName}/${featureName}-${characteristicsName2}.png)`
-        expect(cssFragments[1]).toMatch(urlPath2)
+        expectUrlCorrect(cssFragments[0], featureName, characteristicsName)
+        expectUrlCorrect(cssFragments[1], featureName, characteristicsName2)
+
 
     })
 
