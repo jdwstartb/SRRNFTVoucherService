8904ed72e6d45f509d3ec41a155eac57261622c6
jdwstartb <jens@startbahn.jp>
2022-06-26 19:26:59 +0900

    green multiple characteristics

--- src/build_tools/css-fragment-generator-service.ts
+++ src/build_tools/css-fragment-generator-service.ts
@@ -3,6 +3,13 @@ import {FeatureDefinition} from "./Types";
 export class CSSFragmentGeneratorService {
 
     getCSSFragments(feature: FeatureDefinition): string[] {
-        return [".option-aFeatureName-aCharacteristicName url(./assets/aFeatureName/aFeatureName-aCharacteristicName.png)"]
+        const returnValue: string[] = []
+
+        feature.characteristics.forEach((characteristic) => {
+            returnValue.push(`.option-${feature.featureName}-${characteristic.name} {background-image:  url(./assets/${feature.featureName}/${feature.featureName}-${characteristic.name}.png); }`)
+        })
+
+
+        return returnValue
     }
 }
--- src/build_tools/test/css-fragment-generator-service.spec.ts
+++ src/build_tools/test/css-fragment-generator-service.spec.ts
@@ -5,7 +5,7 @@ describe("CSSFragmentGeneratorService", () => {
 
     const service: CSSFragmentGeneratorService = new CSSFragmentGeneratorService()
 
-    it("generates a css fragment for a single entry", () => {
+    it("generates a css fragment for a single characteristics", () => {
         const featureName = "aFeatureName"
         const characteristicsName = "aCharacteristicName"
 
@@ -22,9 +22,35 @@ describe("CSSFragmentGeneratorService", () => {
         expect(cssFragments[0]).toMatch(className)
         const urlPath = `url(./assets/${featureName}/${featureName}-${characteristicsName}.png)`
         expect(cssFragments[0]).toMatch(urlPath)
-
-        // .option-darkBlue {
-        //         background-image: url(./assets/bg/bg-darkBlue.png);
-        //     }
     })
+
+    it("generates css fragments for multiple characteristics", () => {
+        const featureName = "aFeatureName"
+        const characteristicsName = "aCharacteristicName"
+        const characteristicsName2 = "differntCharacteristicName"
+
+        const testFeature: FeatureDefinition = {
+            featureName,
+            orderNumber: 0,
+            characteristics: [{
+                name: characteristicsName,
+                content: "someIrrelevant Content"
+            }, {name: characteristicsName2, content: "someIrrelevant Content"}]
+        }
+
+        const cssFragments = service.getCSSFragments(testFeature)
+
+        expect(cssFragments.length).toEqual(2)
+        const className = `.option-${featureName}-${characteristicsName}`
+        expect(cssFragments[0]).toMatch(className)
+        const urlPath = `url(./assets/${featureName}/${featureName}-${characteristicsName}.png)`
+        expect(cssFragments[0]).toMatch(urlPath)
+
+        const className2 = `.option-${featureName}-${characteristicsName2}`
+        expect(cssFragments[1]).toMatch(className2)
+        const urlPath2 = `url(./assets/${featureName}/${featureName}-${characteristicsName2}.png)`
+        expect(cssFragments[1]).toMatch(urlPath2)
+
+    })
+
 })
