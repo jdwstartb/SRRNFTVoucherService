05bca009da1a0716c8771b8a20706d3629332682
jdwstartb <jens@startbahn.jp>
2022-07-18 18:26:17 +0900

    cleanup

--- test/jest-global-teardown.ts
+++ test/jest-global-teardown.ts
@@ -1,14 +1,6 @@
-import { execFileSync } from 'child_process'
-
 const main = (): Promise<void> =>
-  new Promise((resolve) => {
-    // Skip database shutdown as circle ci is configured to manage it
-    // (see .circleci/config.yml)
-    if (process.env.CI !== 'true') {
-      execFileSync('./bin/stop-mysql-test')
-      execFileSync('./bin/stop-redis-test')
-    }
-    resolve()
-  })
+    new Promise((resolve) => {
+        resolve()
+    })
 
 export default main
